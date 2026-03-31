7746b0ce79a86d323287fc656466a5f0a97a5460
DaFreeOne <quentin.despretz@gmail.com>
2026-03-31 15:35:51 +0200

    added démarreur to the car

---
+++ demarreur_app.R
@@ -0,0 +1,10 @@
+port <- as.integer(Sys.getenv("SHINY_PORT", "5288"))
+host <- Sys.getenv("SHINY_HOST", "0.0.0.0")
+app_dir <- Sys.getenv("SHINY_APP_DIR", "/app/app")
+
+shiny::runApp(
+  appDir = app_dir,
+  host = host,
+  port = port,
+  launch.browser = FALSE
+)
