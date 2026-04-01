bda6322728c9738361806dba7d852e3828c5c367
DaFreeOne <quentin.despretz@gmail.com>
2026-04-01 19:02:43 +0200

    Updated launcher

--- windows_launcher.bat
+++ windows_launcher.bat
@@ -1,12 +1,19 @@
 @echo off
-start "" /b cmd /c "timeout /t 3 /nobreak >nul && start "" http://localhost:5288/" 
+setlocal
+
+set "BROWSE_DIR=%USERPROFILE%"
+set "OUT_DIR=%USERPROFILE%\shiny_out"
+
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
+
+start "" /b cmd /c "timeout /t 3 /nobreak >nul && start "" http://localhost:5288/"
 
 docker run --rm -p 5288:5288 ^
   -e SHINY_PORT=5288 ^
   -e SHINY_ROOT_PATH=/browse ^
   -e SHINY_ROOT_NAME=home ^
-  --mount type=bind,source="C:\Users",target=/browse ^
-  --mount type=bind,source="C:\shiny_out",target=/out ^
+  --mount type=bind,source="%BROWSE_DIR%",target=/browse ^
+  --mount type=bind,source="%OUT_DIR%",target=/out ^
   qtea1/bulktools:latest
 
 pause
